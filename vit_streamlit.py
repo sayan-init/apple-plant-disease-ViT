785b66612176e04ff9f332b0429516ba474ac739
Sayan Mondal <smsayan09@gmail.com>
2026-03-20 01:55:34 +0530

    update streamlit

--- vit_streamlit.py
+++ vit_streamlit.py
@@ -67,37 +67,3 @@ if uploaded_image is not None:
         # Get probable cure
         cure = get_probable_remedy(prediction_result)
         st.info(f"Probable Cure: {cure}")
-
-import numpy as np
-# import tensorflow as tf
-
-loaded_model = tf.keras.models.load_model('/content/drive/MyDrive/Apple_Plant_DD_ViT/vit_al9ee')
-loaded_model.summary()
-
-IMG_WIDTH = 120
-IMG_HEIGHT = 120
-BATCH_SIZE = 100
-
-TESTING_DATASET_DIR = '/content/drive/MyDrive/Apple_Plant_DD_ViT/dataset/Test'
-dataset = tf.keras.utils.image_dataset_from_directory(
-  TESTING_DATASET_DIR,
-  image_size=(IMG_HEIGHT, IMG_WIDTH),
-  batch_size=BATCH_SIZE
-)
-
-correctly_predicted = 0
-total_images = 0
-for step, (images, labels) in enumerate(dataset):
-  actual_results = labels.numpy()
-  predictions = loaded_model(images)
-  for j in range(len(predictions.numpy())):
-    total_images += 1
-    pd = predictions[j]
-    max_i = 0
-    for i in range(len(pd)):
-        if pd[i] > pd[max_i]:
-            max_i = i
-    if max_i == actual_results[j]:
-        correctly_predicted += 1
-
-print(f"accuracy -> {correctly_predicted / total_images}")
